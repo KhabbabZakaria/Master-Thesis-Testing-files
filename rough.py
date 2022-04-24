b29f8d13496994fa684c173f9cdcf2353f636c51
Khabbab Zakaria <zakariak.engg@gmail.com>
2022-04-24 13:48:18 +0200

    AAPL max on a day/ diff with corresponding markets

--- rough.py
+++ rough.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from data import *
 from actorcritic import *
 from parameters import *
@@ -18,7 +19,9 @@ current_dir = os.getcwd()
 ref_folder = os.path.join(current_dir, 'preprocessed_dir')
 files = [name for name in os.listdir(ref_folder) if name!= 'vwap calculation.py']
 
-
+check_max = True
+check_max_minus_corresponding = False
+#check_avgbest_minus_corresponding = False
 
 def calc_twap(df_csv):
     n = len(df_csv) - 1
@@ -53,45 +56,80 @@ def calc_vwap(df_csv):
 file = files[ref_stock]
 #print('........................................................')
 #print(file)
-df = pd.read_csv(os.path.join(ref_folder, file))
-df = df.iloc[:T*50]
-print(df.iloc[T*15:T*16])
-
-volume = df['volume'].iloc[T*15:T*16].tolist()
-Q = sum(volume)*0.01
-
-
-price = df['vwap'].iloc[T*15:T*16].tolist()
-
-
-volumes = df['volume'].iloc[T*15:T*16]
-volume_list = volumes.values.tolist()
-
-
-weight_vwap_list = []
-for i in range(len(volume_list)):
-    weight_vwap_list.append(volume_list[i]/sum(volume_list))
+def Nmaxelements(list1, N):
+    final_list = []
+  
+    for i in range(0, N): 
+        max1 = -10000000
+          
+        for j in range(len(list1)):     
+            if list1[j] > max1:
+                max1 = list1[j]
+        list1.remove(max1)
+        final_list.append(max1)
+          
+    return final_list
 
 
-vwaplist = []
-for i in range(len(df.iloc[T*15:T*16])):
-    q = Q*weight_vwap_list[i]
-    vwaplist.append(q)
+vwapfinal = []
+modelfinal = []
+for a in range(20):
+    #a = 1
+    df = pd.read_csv(os.path.join(ref_folder, file))
+    df = df.iloc[:T*50]
+
+    volume = df['volume'].iloc[T*a:T*(a+1)].tolist()
+    Q = sum(volume)*0.01
 
 
 
+    price = df['vwap'].iloc[T*a:T*(a+1)].tolist()
 
-modellist = []
-for k in range(1,2):
-    PATH = 'seed_' + str(k) + '_model.pt'
-    #print(PATH)
 
-    agent = Agent(model, is_eval=True, model_name=PATH)
-    env.reset()
-    state, _ = getState(all_stressed_test_list[-16], all_stressed_test_vwap_list[-16], env.time, state_size)
+    volumes = df['volume'].iloc[T*a:T*(a+1)]
+    volume_list = volumes.values.tolist()
 
-    for j in range(state_size, len((all_stressed_test_list[15]))):
-        if j%10==0:
+
+    weight_vwap_list = []
+    for i in range(len(volume_list)):
+        weight_vwap_list.append(volume_list[i]/sum(volume_list))
+
+
+    vwaplist = []
+    for i in range(len(df.iloc[T*a:T*(a+1)])):
+        q = Q*weight_vwap_list[i]*price[i]
+        vwaplist.append(q)
+
+    index = vwaplist.index(max(vwaplist))
+
+    vwaplist2 = []
+    for n in range(len(vwaplist)):
+        vwaplist2.append(volume_list[n] - vwaplist[n])
+        
+
+
+    if check_max == True:
+        vwapfinal.append(max(vwaplist))
+        y_label = 'Maximum Volume Sold of all the Minutes on a Day'
+    elif check_max_minus_corresponding == True:
+        vwapfinal.append(volume_list[index] - vwaplist[index])
+        y_label = 'Corresponding Market Sale - Sale by Strategy on Most Extreme Case'
+    else:
+        vwapfinal.append(sum(Nmaxelements(vwaplist2, 39))/39)
+        y_label = 'Average(Market Sale -  Sale by Strategy) on 10% of Extreme Cases'
+    #vwapfinal.append(volume_list[index] - vwaplist[index])
+
+    #vwapfinal.append(sum(Nmaxelements(vwaplist, 10))/10)
+
+    modellist = []
+    for k in range(1,2):
+        PATH = 'seed_' + str(k) + '_model.pt'
+        #print(PATH)
+
+        agent = Agent(model, is_eval=True, model_name=PATH)
+        env.reset()
+        state, _ = getState(all_stressed_test_list[-(a+1)], all_stressed_test_vwap_list[-(a+1)], env.time, state_size)
+        for j in range(state_size, len((all_stressed_test_list[15]))):
             already_bought_tensor = torch.Tensor(env.already_bought_list)
             left_time = T - env.time
             left_time_tensor = torch.Tensor(list(range(left_time+1,left_time+1+state_size)))
@@ -99,38 +137,31 @@ for k in range(1,2):
             private_input = private_input.to(device)
             state = state.to(device)
             action, _ = agent.act(private_input, state)
-            for _ in range(10):
-             modellist.append((action*Q).item())
+            modellist.append((action*Q*price[j]).item())
 
-modellist = modellist[::-1]
+    index = modellist.index(max(modellist))
 
-x = list(range(len(vwaplist)))
-coef_vwap = np.polyfit(x,vwaplist,10)
-poly1d_fn_vwap = np.poly1d(coef_vwap) 
+    modellist2 = []
+    for n in range(len(modellist)):
+        modellist2.append(volume_list[n] - modellist[n])
+    modelfinal.append(sum(Nmaxelements(modellist2, 39))/39)
 
-x = list(range(len(modellist)))
-coef_model = np.polyfit(x,modellist,5)
-poly1d_fn_model = np.poly1d(coef_model) 
+    #modelfinal.append(volume_list[index] - modellist[index])
+    #modelfinal.append(sum(Nmaxelements(modellist, 10))/10)
+print('.......................................................................................')
 
+'''
+vwapmax = max(vwapfinal)
+modelmax = max(modelfinal)
 
+print(vwapmax, modelmax)'''
 
 import matplotlib.pyplot as plt
-
-x = list(range(len(volume_list)))
-fig, ax1 = plt.subplots()
-ax2 = ax1.twinx()
-ax1.plot(price, 'g--', label = 'price')
-ax2.plot(x, poly1d_fn_vwap(x), 'b-', label = 'vwap')
-ax2.plot(x, poly1d_fn_model(x), 'r-', label = 'DRL')
-ax2.fill_between(
-    x, volume_list, alpha=0.25, 
-    label="Total trade in the market"
-)
-
-ax1.set_xlabel('Minute of the Day')
-ax1.set_ylabel('Price of the Stock in USD', color='g')
-ax2.set_ylabel('Stocks to sell at the minute', color='k')
-ax2.axis(ymin=0, ymax=10000)
+plt.plot(vwapfinal, label = 'vwap', color='blue', linestyle='dashed', marker='o')
+plt.plot(modelfinal, label = 'DRL', color='red', linestyle='dashed', marker='o')
 plt.legend()
-plt.title('FB on 2022-02-16')
+plt.ylabel(y_label)
+plt.xlabel('20 Trading Days')
+plt.xticks([], minor=True)
+plt.title('AAPL during Inflation+War')
 plt.show()
